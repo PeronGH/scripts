37263bb0132e72ccc5282955944f1cb3d64a8603
PeronGH <peron_ap@icloud.com>
2025-02-03 20:00:09 +0000

    Add check chatwise

---
+++ check_chatwise.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -s https://chatwise.app/api/trpc/getReleases | jq -r '[ .result.data[] ] | reverse[] | "Version: \(.name)\nTime: \(.publishedAt | fromdate | strftime("%Y-%m-%d %H:%M:%S"))\nChangelog:\n\(.body)\n---"'
