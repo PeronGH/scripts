ef0b8753a71ab3e2b365b256677a2ca9d780b869
Peron <peron_ap@icloud.com>
2025-06-13 23:23:46 +0100

    feat: show 8280_ARC qcdx version

--- check/qcdx8280.sh
+++ check/qcdx8280.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-curl -s https://raw.githubusercontent.com/WOA-Project/Qualcomm-Reference-Drivers/refs/heads/master/8280_QRD/CHANGELOG.md | grep "qcdx8280.cab" | awk '{last=$0} END {print $2}'
+
+grep_ver() {
+    curl -s "$1" | grep "qcdx8280.cab" | awk '{last=$0} END {print $2}'
+}
+
+echo -n "8280_QRD: "
+grep_ver https://raw.githubusercontent.com/WOA-Project/Qualcomm-Reference-Drivers/refs/heads/master/8280_QRD/CHANGELOG.md
+
+echo -n "8280_ARC: "
+grep_ver https://raw.githubusercontent.com/WOA-Project/Qualcomm-Reference-Drivers/refs/heads/master/Surface/8280_ARC_1997/CHANGELOG.md
