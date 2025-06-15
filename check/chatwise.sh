61b18a15864c30a34a95614952e7e6591db9c8f9
Peron <peron_ap@icloud.com>
2025-06-15 02:11:39 +0100

    format script

--- check/chatwise.sh
+++ check/chatwise.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -s https://chatwise.app/api/trpc/getReleases | jq -r '[ .result.data[] ] | reverse[] | "Version: \(.name)\nTime: \(.publishedAt | fromdate | strftime("%Y-%m-%d %H:%M:%S"))\nChangelog:\n\(.body)\n---"'
+curl -s https://chatwise.app/api/trpc/getReleases |
+    jq -r '[ .result.data[] ] | reverse[] | "Version: \(.name)\nTime: \(.publishedAt | fromdate | strftime("%Y-%m-%d %H:%M:%S"))\nChangelog:\n\(.body)\n---"'
