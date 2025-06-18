d8eedf45a425ec41e3fef5e14aa0f9709b58e527
Peron <peron_ap@icloud.com>
2025-06-18 18:43:46 +0100

    feat: cleaner version and model ids

--- check/gemini-test-models.sh
+++ check/gemini-test-models.sh
@@ -2,6 +2,5 @@
 
 script=$(curl -s https://update.greasyfork.org/scripts/539399/Google%20AI%20Studio%20%E6%A8%A1%E5%9E%8B%E6%B3%A8%E5%85%A5%E5%99%A8.user.js)
 
-echo "$script" | grep "VERSION:"
-echo "$script" | sed -n '/const PREDEFINED_MODELS = \[/,/\];/p'
-echo "$script" | sed -n '/const newModels = \[/,/\];/p'
+echo "$script" | grep -oE 'VERSION: (.+)"'
+echo "$script" | grep -oE '"models/.+"'
